51140650af843d4e3a82f66d9a7a8ec4df70fcbd
Juan Acuña <juca.acuna@gmail.com>
2024-12-26 15:23:19 -0300

    Formulario y consulta prontos. Se agregan valores para poblar la base de datos.

--- extras/crear.sql
+++ extras/crear.sql
@@ -16,3 +16,11 @@ CREATE TABLE News (
     body TEXT,
     dateTime DATETIME NOT NULL
 );
+
+
+INSERT INTO News (title, body, dateTime) VALUES
+('Insolita navidad en la UOC', 'Estudiante uruguayo de la UOC quemo el asado por realizar tarea del curso de ciber seguridad durante la jornada de navidad. La familia muy disgustada le corto el internet.', '2024-12-25 20:00:00'),
+('Trucos tecnologicos', 'Cuando el PC no funcione, desnchufe y vuelva a conectar. Asi, al inciar el PC, vera que el equipo funcionara aun mejor que antes.', '2022-10-12 21:00:00'),
+('Consejos de salud', 'Mientra codifica en la notebook, no coma dulces, ni salados. Tampoco amargos, ni acidos. Sencillamente no coma. Esto mejorara su estado fisico, reduciendo el peso. Como plus, vera que esto le permite concentrarse mas.', '2023-10-13 22:00:00'),
+('Resumen deportivo', 'Confirmado, Nvidia le gano a Intel 1 a 0.', '2024-11-14 23:00:00'),
+('Noticias de la UOC', 'La UOC ha decidido realizar un reconocimiento a aquellos estudiantes que en codearon durante navidad.', '2024-09-15 00:00:00');
---
+++ src/www/miMenu.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Este archivo contiene el menú de navegación, el cual es importado en cada página.
+ */
+echo
+    '<header>
+        <h1>Noticias sobre SQLi</h1>
+    </header>
+    <nav>
+        <ul>
+            <li> <a href="./">INICIO</a></li>
+        </ul>
+    </nav>
+    ';
+    
+    
+
+
+?>
